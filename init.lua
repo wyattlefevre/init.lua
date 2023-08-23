2908a02db9a46f1a39e2921e4b15474862e721c1
Wyatt <wyatt@pop-os.localdomain>
2023-08-23 16:03:28 -0600

    no swap file, push to github early and often

--- init.lua
+++ init.lua
@@ -204,8 +204,8 @@ vim.o.smartcase = true
 vim.wo.signcolumn = 'yes'
 
 -- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
+-- vim.o.updatetime = 250
+-- vim.o.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
@@ -213,7 +213,7 @@ vim.o.completeopt = 'menuone,noselect'
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
 
-vim.o.noswapfile = true
+vim.o.swapfile = true
 
 -- [[ Basic Keymaps ]]
 
