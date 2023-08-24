c62e6507c11b318662aa496bff6934948f07c4ec
Wyatt <wyatt@pop-os.localdomain>
2023-08-23 18:33:29 -0600

    essential split and window shortcuts

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -5,8 +5,17 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+-- Switch panes
 vim.keymap.set('n', '<S-h>', "<C-w>h", default_opts)
 vim.keymap.set('n', '<S-l>', "<C-w>l", default_opts)
+
+-- Close pane
+vim.keymap.set('n', '<C-w>', ':close<CR>', default_opts)
+
+-- Vertical Split
+vim.keymap.set("n", "<C-v>", "<C-w>v", default_opts)
+vim.keymap.set("n", "<C-c>l", "<C-w>v<C-w>l:close<CR>", default_opts)
+vim.keymap.set("n", "<C-c>h", "<C-w>v<C-w>h<C-w>h:close<CR><C-w>l", default_opts)
 
 -- Clear highlight (search) with esc
 vim.keymap.set('n', '<ESC>', ':noh<CR>', default_opts)
