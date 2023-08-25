6619011458969e5edb3cbc44307db2b01bb0216c
Wyatt <wyatt@pop-os.localdomain>
2023-08-25 10:30:50 -0600

    keybindings and small filetree change

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -21,12 +21,15 @@ vim.keymap.set("n", "<C-c>h", "<C-w>v<C-w>h<C-w>h:close<CR><C-w>l", default_opts
 vim.keymap.set('n', '<ESC>', ':noh<CR>', default_opts)
 
 -- Center screen on vertical movement
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
-vim.keymap.set('v', '<C-d>', '<C-d>zz')
-vim.keymap.set('v', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<C-d>', '<C-d>zz', default_opts)
+vim.keymap.set('n', '<C-u>', '<C-u>zz', default_opts)
+vim.keymap.set('v', '<C-d>', '<C-d>zz', default_opts)
+vim.keymap.set('v', '<C-u>', '<C-u>zz', default_opts)
 
 vim.keymap.set('n', 'n', 'nzz')
 vim.keymap.set('n', 'N', 'Nzz')
 
 vim.keymap.set('n', 'G', 'Gzz')
+
+-- Refresh buffer
+vim.keymap.set('n', '<leader>rb', ':e!<CR>')
--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -47,10 +47,11 @@ return {
     vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
     vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
     vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
-    vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
-    vim.keymap.set('n', '<leader>B', function()
-      dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-    end, { desc = 'Debug: Set Breakpoint' })
+    -- TODO: replace these keymaps when I want to start using a debugger
+    -- vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+    -- vim.keymap.set('n', '<leader>B', function()
+    --   dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+    -- end, { desc = 'Debug: Set Breakpoint' })
 
     -- Dap UI setup
     -- For more information, see |:help nvim-dap-ui|
