62244d2297a7d3abc9cb4c36bc64fc104dd33db3
Wyatt <wyattlefevre@gmail.com>
2023-08-29 06:46:22 -0600

    rewrite with lua instead of monster string

--- init.lua
+++ init.lua
@@ -321,7 +321,8 @@ end, { desc = '[/] Fuzzily search in current buffer' })
 
 vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
 vim.keymap.set('n', '<leader>f',
-  "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '-u', '--files', '--hidden', '-g', '!.git', '-g', '!node_modules/**', '-g', '!.next/**'}})<cr>",
+  function() require 'telescope.builtin'.find_files({
+      find_command = { 'rg', '-u', '--files', '--hidden', '-g', '!.git', '-g', '!node_modules/**', '-g', '!.next/**' } }) end,
   { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
