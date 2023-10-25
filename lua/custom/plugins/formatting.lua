7fdd62f2d512722fcb0d38803aa8e120fa0e6e79
Wyatt <wyattlefevre@gmail.com>
2023-10-24 20:37:49 -0600

    formatting works!

---
+++ lua/custom/plugins/formatting.lua
@@ -0,0 +1,29 @@
+-- https://www.youtube.com/watch?v=ybUE4D80XSk&ab_channel=JoseanMartinez
+return {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+    conform.setup({
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+      }
+    })
+    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+      })
+    end)
+  end
+}
