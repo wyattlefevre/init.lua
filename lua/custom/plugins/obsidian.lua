54854b900bd2c14147332ac16556dea4d03a4983
Wyatt <wyatt@neighbor.com>
2023-12-14 07:37:50 -0700

    add obsidian plugin

---
+++ lua/custom/plugins/obsidian.lua
@@ -0,0 +1,23 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "Wydocs",
+        path = "~/syncthing/Wydocs2",
+      },
+    },
+    mappings = {
+      ["gd"] = {
+        action = function()
+          return require("obsidian").util.gf_passthrough()
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      }
+    }
+  }
+}
