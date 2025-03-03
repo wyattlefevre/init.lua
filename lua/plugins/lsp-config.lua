5ea9ea584344bad88a175b291a49d3f9b45ecf9d
Wyatt <wyatt@Wyatts-MacBook-Pro.local>
2025-03-03 13:13:47 -0700

    telescope is the way

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -26,7 +26,7 @@ return {
 				nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 				nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
-				nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+				nmap("gd", telescope.lsp_definitions, "[G]oto [D]efinition")
 				nmap("gr", telescope.lsp_references, "[G]oto [R]eferences")
 				nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
 				nmap("gt", vim.lsp.buf.type_definition, "Type [D]efinition")
