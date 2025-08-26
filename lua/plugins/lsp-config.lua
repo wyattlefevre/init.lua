12c54d52619e07d1b146fbfa05b5956116eb1735
Wyatt LeFevre <wyatt.lefevre@getflex.com>
2025-08-26 15:48:44 -0600

    rip out java/jdtls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -61,9 +61,8 @@ return {
 					},
 				},
 			}
-			-- ensure that we have lua language server, typescript launguage server, java language server, and java test language server are installed
 			require("mason-lspconfig").setup({
-				ensure_installed = { "ts_ls", "eslint", "lua_ls", "jdtls" },
+				ensure_installed = { "ts_ls", "eslint", "lua_ls" },
 			})
 
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -79,76 +78,10 @@ return {
 				})
 			end
 
-			-- Custom on_attach for jdtls to add java specific keymaps
-			local jdtls_on_attach = function(client, bufnr)
-				on_attach(client, bufnr)
-				-- java specific keymaps
-				vim.keymap.set("n", "<leader>do", require("jdtls").organize_imports, { buffer = bufnr, desc = "Organize Imports" })
-				vim.keymap.set("n", "<leader>dt", require("jdtls").test_class, { buffer = bufnr, desc = "Test Class" })
-				vim.keymap.set("n", "<leader>dn", require("jdtls").test_nearest_method, { buffer = bufnr, desc = "Test Nearest Method" })
-				vim.keymap.set("n", "gd", require("jdtls").goto_definition, { buffer = bufnr, desc = "Goto Definition" })
-			end
-
-			-- jdtls setup
-			-- This is a bit complex, we need to specify the path to the jdtls launcher and configuration
-			-- We also create a workspace directory for each project.
-			local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
-			local workspace_dir = vim.fn.stdpath("data") .. "/jdtls-workspace/" .. project_name
-			local jdtls_path = vim.fn.stdpath("data") .. "/mason/packages/jdtls"
-
-			local config = {
-				cmd = {
-					"java",
-					"-Declipse.application=org.eclipse.jdt.ls.core.id1.JavaLanguageServerImpl",
-					"-Dosgi.bundles.defaultStartLevel=4",
-					"-Declipse.product=org.eclipse.jdt.ls.core.product",
-					"-Dlog.protocol=true",
-					"-Dlog.level=ALL",
-					"-Xms1g",
-					"--add-modules=ALL-SYSTEM",
-					"--add-opens", "java.base/java.util=ALL-UNNAMED",
-					"--add-opens", "java.base/java.lang=ALL-UNNAMED",
-					"-jar", vim.fn.glob(jdtls_path .. "/plugins/org.eclipse.equinox.launcher_*.jar"),
-					"-configuration", jdtls_path .. "/config_mac", -- mac specific
-					"-data", workspace_dir,
-				},
-				root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
-				on_attach = jdtls_on_attach,
-				capabilities = capabilities,
-				settings = {
-					java = {
-						-- configuration for java formatting
-						format = {
-							settings = {
-								-- get the url for the google style guide
-								url = vim.fn.stdpath("config") .. "/intellij-java-google-style.xml",
-								-- make sure it is enabled
-								enabled = true,
-							}
-						}
-					}
-				}
-			}
-
-			-- We need to start jdtls on the filetype event
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = "java",
-				callback = function()
-					require("jdtls").start_or_attach(config)
-				end,
-			})
-
 			require("mason-lspconfig").setup()
 		end,
 	},
-	
-	-- utility plugin for configuring the java language server for us
-	{
-		"mfussenegger/nvim-jdtls",
-		dependencies = {
-			"mfussenegger/nvim-dap",
-		},
-	},
+
 	{
 		-- LSP Configuration & Plugins
 		"neovim/nvim-lspconfig",
@@ -159,7 +92,7 @@ return {
 
 			-- Useful status updates for LSP
 			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-			{ "j-hui/fidget.nvim", tag = "legacy", opts = {} },
+			{ "j-hui/fidget.nvim",       tag = "legacy", opts = {} },
 
 			-- Additional lua configuration, makes nvim stuff amazing!
 			"folke/neodev.nvim",
